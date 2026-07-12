330be3a37874b4a25bb2174b741051ded87df5b3
Shelby Deerman <srdeerman@crimson.ua.edu>
2026-07-11 23:44:26 -0500

    lab completed

--- labs/lab05-api-client-and-db-intg/starter/client/app.js
+++ labs/lab05-api-client-and-db-intg/starter/client/app.js
@@ -5,7 +5,16 @@ const itemList = document.querySelector("#items");
 const form = document.querySelector("#add-item-form");
 const itemNameInput = document.querySelector("#item-name");
 const itemQuantityInput = document.querySelector("#item-quantity");
+const itemCategoryInput = document.querySelector("#item-category");
 const statusBox = document.querySelector("#status");
+
+const updateForm = document.querySelector("#update-item-form");
+const updateIdInput = document.querySelector("#update-id");
+const updateItemNameInput = document.querySelector("#update-item-name");
+const updateItemQuantityInput = document.querySelector("#update-item-quantity");
+const updateItemCategoryInput = document.querySelector("#update-item-category");
+const deleteForm = document.querySelector("#delete-item-form");
+const deleteIDInput = document.querySelector("#delete-id");
 
 function setStatus(message) {
   statusBox.textContent = message;
@@ -16,7 +25,7 @@ function renderItems(items) {
 
   for (const item of items) {
     const li = document.createElement("li");
-    li.textContent = `${item.id}: ${item.name} (${item.quantity})`;
+    li.textContent = `${item.id}: ${item.name} (${item.quantity}) (Category ${item.category_id}: ${item.category})`;
     itemList.appendChild(li);
   }
 }
@@ -39,7 +48,25 @@ async function loadItems() {
   }
 }
 
-async function addItem(name, quantity) {
+async function loadItemByID(id) {
+  setStatus("Loading item ...");
+
+  try {
+    const response = await fetch(`${API_BASE_URL}/api/items/${id}`);
+
+    if (!response.ok) {
+      throw new Error(`GET /api/items/:id failed with status ${response.status}`);
+    }
+
+    const data = await response.json();
+    renderItems(data.items);
+    setStatus("Items loaded.");
+  } catch (error) {
+    setStatus(error.message);
+  }
+}
+
+async function addItem(name, quantity, category) {
   setStatus("Adding item...");
 
   try {
@@ -48,7 +75,7 @@ async function addItem(name, quantity) {
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({ name, quantity })
+      body: JSON.stringify({ name, quantity, category })
     });
 
     const data = await response.json();
@@ -64,6 +91,50 @@ async function addItem(name, quantity) {
   }
 }
 
+async function replaceItem(id, name, quantity, category) {
+  setStatus("Replacing item...");
+
+  try {
+    const response = await fetch(`${API_BASE_URL}/api/items/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ name, quantity, category })
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message ?? `PUT /api/items failed with status ${response.status}`);
+    }
+
+    setStatus(`Replaced item: ${id}`);
+    await loadItems();
+  } catch (error) {
+    setStatus(error.message);
+  }
+}
+
+async function deleteItem(id) {
+  setStatus("Deleting item...");
+
+  try {
+    const response = await fetch(`${API_BASE_URL}/api/items/${id}`, {
+      method: "DELETE",
+    });
+
+    if (!response.ok) {
+      throw new Error(`DELETE /api/items/:id failed with status ${response.status}`);
+    }
+
+    await loadItems();
+    setStatus("Item Deleted.");
+  } catch (error) {
+    setStatus(error.message);
+  }
+}
+
 loadButton.addEventListener("click", loadItems);
 
 form.addEventListener("submit", async (event) => {
@@ -71,13 +142,54 @@ form.addEventListener("submit", async (event) => {
 
   const name = itemNameInput.value.trim();
   const quantity = Number(itemQuantityInput.value);
+  const category = Number(itemCategoryInput.value);
 
-  if (!name || !Number.isInteger(quantity) || quantity < 0) {
-    setStatus("Enter a name and a non-negative integer quantity.");
+  if (!name || !Number.isInteger(quantity) || quantity < 0 || !Number.isInteger(category) || category < 0) {
+    setStatus("Enter a name and a non-negative integer quantity and category.");
     return;
   }
 
   itemNameInput.value = "";
   itemQuantityInput.value = "0";
-  await addItem(name, quantity);
+  itemCategoryInput.value = "0";
+  await addItem(name, quantity, category);
+});
+
+deleteForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const deleteID = Number(deleteIDInput.value);
+
+  if (!Number.isInteger(deleteID) || deleteID < 0) {
+    setStatus("Enter a non-negative integer ID.");
+    return;
+  }
+
+  deleteIDInput.value = "0";
+  await deleteItem(deleteID);
+});
+
+updateForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const updateId = Number(updateIdInput.value);
+  const updateName = updateItemNameInput.value.trim();
+  const updateQuantity = Number(updateItemQuantityInput.value);
+  const updateCategory = Number(updateItemCategoryInput.value);
+
+  if (!Number.isInteger(updateId) || updateId < 0) {
+    setStatus("Enter a non-negative integer ID.");
+    return;
+  }
+
+  if (!updateName || !Number.isInteger(updateQuantity) || updateQuantity < 0 || !Number.isInteger(updateCategory) || updateCategory < 0) {
+    setStatus("Enter a name and a non-negative integer quantity and category.");
+    return;
+  }
+
+  updateIdInput.value = "0";
+  updateItemNameInput.value = "";
+  updateItemQuantityInput.value = "0";
+  updateItemCategoryInput.value = "0";
+  await replaceItem(updateId, updateName, updateQuantity, updateCategory);
 });
--- labs/lab05-api-client-and-db-intg/starter/src/server.js
+++ labs/lab05-api-client-and-db-intg/starter/src/server.js
@@ -43,8 +43,9 @@ export function createApp() {
   app.get("/api/items", async (req, res) => {
     try {
       const result = await pool.query(`
-        SELECT id, name, quantity
+        SELECT items.id, items.name, items.quantity, items.category_id, categories.name AS category
         FROM items
+        JOIN categories ON items.category_id = categories.id
         ORDER BY id ASC
       `);
 
@@ -62,22 +63,36 @@ export function createApp() {
   app.post("/api/items", async (req, res) => {
     const name = req.body?.name?.trim();
     const quantity = Number(req.body?.quantity);
+    const category = Number(req.body?.category);
 
-    if (!name || !Number.isInteger(quantity) || quantity < 0) {
+    if (!name || !Number.isInteger(quantity) || quantity < 0 || !Number.isInteger(category) || category < 0) {
       return res.status(400).json({
         error: "Bad Request",
-        message: "A name and non-negative integer quantity are required."
+        message: "A name and non-negative integer quantity and category are required."
       });
     }
 
     try {
+      const categoryResult = await pool.query(
+        `SELECT id 
+        FROM categories
+        WHERE id = $1`,
+        [category]
+      );
+
+      if (categoryResult.rows.length === 0) {
+        return res.status(400).json({
+          error: "Invalid category ID"
+        });
+      }
+
       const result = await pool.query(
         `
-          INSERT INTO items (name, quantity)
-          VALUES ($1, $2)
-          RETURNING id, name, quantity
+          INSERT INTO items (name, quantity, category_id)
+          VALUES ($1, $2, $3)
+          RETURNING id, name, quantity, category_id
         `,
-        [name, quantity]
+        [name, quantity, category]
       );
 
       res.status(201).json({ item: result.rows[0] });
@@ -90,24 +105,179 @@ export function createApp() {
     }
   });
 
-  // TODO: Return one item by ID.
-  app.get("/api/items/:id", (req, res) => {
-    res.status(501).json({ error: "Not implemented yet" });
+  app.get("/api/items/:id", async (req, res) => {
+    const requestedID = Number(req.params.id);
+    try {
+      const result = await pool.query(`
+        SELECT id, name, quantity, category_id
+        FROM items
+        WHERE id = $1`,
+        [requestedID]
+      );
+
+      res.json({ items: result.rows });
+    } catch (error) {
+      console.error("Failed to load items:", error);
+      res.status(500).json({
+        error: "Internal Server Error",
+        message: "Failed to load items."
+      });
+    }
   });
 
-  // TODO: Replace one item by ID.
-  app.put("/api/items/:id", (req, res) => {
-    res.status(501).json({ error: "Not implemented yet" });
+  app.put("/api/items/:id", async (req, res) => {
+    const requestedID = Number(req.params.id);
+    const name = req.body?.name?.trim();
+    const quantity = Number(req.body?.quantity);
+    const category = Number(req.body?.category);
+
+    if (!name || !Number.isInteger(quantity) || quantity < 0 || !Number.isInteger(category) || category < 0) {
+      return res.status(400).json({
+        error: "Bad Request",
+        message: "A name and non-negative integer quantity and category are required."
+      });
+    }
+
+    try {
+      const categoryResult = await pool.query(
+        `SELECT id 
+        FROM categories
+        WHERE id = $1`,
+        [category]
+      );
+
+      if (categoryResult.rows.length === 0) {
+        return res.status(400).json({
+          error: "Invalid category ID"
+        });
+      }
+
+      const result = await pool.query(`
+        UPDATE items
+        SET name = $1, quantity = $2, category_id = $3
+        WHERE id = $4`,
+        [name, quantity, category, requestedID]
+      );
+
+      res.json({ items: result.rows });
+    } catch (error) {
+      console.error("Failed to load items:", error);
+      res.status(500).json({
+        error: "Internal Server Error",
+        message: "Failed to load items."
+      });
+    }
   });
 
-  // TODO: Partially update one item by ID.
-  app.patch("/api/items/:id", (req, res) => {
-    res.status(501).json({ error: "Not implemented yet" });
+  app.patch("/api/items/:id", async (req, res) => {
+    const requestedID = Number(req.params.id);
+    if ("name" in req.body) {
+      const name = req.body?.name?.trim();
+      if (!name) {
+        return res.status(400).json({
+          error: "Bad Request",
+          message: "A name is required."
+        });
+      }
+      try {
+        const result = await pool.query(`
+          UPDATE items
+          SET name = $1
+          WHERE id = $2`,
+          [name, requestedID]
+        );
+
+        res.json({ items: result.rows });
+      } catch (error) {
+        console.error("Failed to load items:", error);
+        res.status(500).json({
+          error: "Internal Server Error",
+          message: "Failed to load items."
+        });
+      }
+    }
+    if ("quantity" in req.body) {
+      const quantity = Number(req.body?.quantity);
+      if (!Number.isInteger(quantity) || quantity < 0) {
+        return res.status(400).json({
+          error: "Bad Request",
+          message: "A non-negative integer quantity is required."
+        });
+      }
+      try {
+        const result = await pool.query(`
+          UPDATE items
+          SET quantity = $1
+          WHERE id = $2`,
+          [quantity, requestedID]
+        );
+
+        res.json({ items: result.rows });
+      } catch (error) {
+        console.error("Failed to load items:", error);
+        res.status(500).json({
+          error: "Internal Server Error",
+          message: "Failed to load items."
+        });
+      }
+    }
+    if ("category" in req.body) {
+      const category = Number(req.body?.category);
+      if (!Number.isInteger(category) || category < 0) {
+        return res.status(400).json({
+          error: "Bad Request",
+          message: "A non-negative integer category is required."
+        });
+      }
+      try {
+        const categoryResult = await pool.query(
+          `SELECT id 
+          FROM categories
+          WHERE id = $1`,
+          [category]
+        );
+
+        if (categoryResult.rows.length === 0) {
+          return res.status(400).json({
+            error: "Invalid category ID"
+          });
+        }
+
+        const result = await pool.query(`
+          UPDATE items
+          SET category_id = $1
+          WHERE id = $2`,
+          [category, requestedID]
+        );
+
+        res.json({ items: result.rows });
+      } catch (error) {
+        console.error("Failed to load items:", error);
+        res.status(500).json({
+          error: "Internal Server Error",
+          message: "Failed to load items."
+        });
+      }
+    }
   });
 
-  // TODO: Delete one item by ID.
-  app.delete("/api/items/:id", (req, res) => {
-    res.status(501).json({ error: "Not implemented yet" });
+  app.delete("/api/items/:id", async (req, res) => {
+    const requestedID = Number(req.params.id);
+    try {
+      const result = await pool.query(`
+        DELETE FROM items
+        WHERE id = $1`,
+        [requestedID]
+      );
+
+      res.status(204).json({ status: "Successfully deleted" });
+    } catch (error) {
+      console.error("Failed to load items:", error);
+      res.status(500).json({
+        error: "Internal Server Error",
+        message: "Failed to load items."
+      });
+    }
   });
 
   app.use((req, res) => {
@@ -119,10 +289,30 @@ export function createApp() {
 
 export async function initializeDatabase() {
   await pool.query(`
+    CREATE TABLE IF NOT EXISTS categories (
+      id SERIAL PRIMARY KEY,
+      name TEXT NOT NULL
+    )
+  `);
+
+  const result = await pool.query("SELECT COUNT(*)::int AS count FROM categories");
+
+  if (result.rows[0].count === 0) {
+    await pool.query(
+      `
+        INSERT INTO categories (name)
+        VALUES ($1), ($2)
+      `,
+      ["Tech Supplies", "Office Supplies"]
+    );
+  }
+
+  await pool.query(`
     CREATE TABLE IF NOT EXISTS items (
       id SERIAL PRIMARY KEY,
       name TEXT NOT NULL,
-      quantity INTEGER NOT NULL CHECK (quantity >= 0)
+      quantity INTEGER NOT NULL CHECK (quantity >= 0),
+      category_id INTEGER NOT NULL REFERENCES categories(id)
     )
   `);
 
@@ -131,18 +321,26 @@ export async function initializeDatabase() {
   if (rows[0].count === 0) {
     await pool.query(
       `
-        INSERT INTO items (name, quantity)
-        VALUES ($1, $2), ($3, $4), ($5, $6)
+        INSERT INTO items (name, quantity, category_id)
+        VALUES ($1, $2, $3), ($4, $5, $6), ($7, $8, $9)
       `,
-      ["Keyboard", 10, "Mouse", 5, "Monitor", 3]
+      ["Keyboard", 10, 1, "Mouse", 5, 1, "Monitor", 3, 1]
     );
   }
 }
 
 const isMainModule = process.argv[1] === new URL(import.meta.url).pathname;
 
-if (isMainModule) {
+// if (isMainModule) {
   const app = createApp();
+
+  // if database has already been initialized without my "category" column added,
+  // uncomment the below code out to reset the tables.
+
+  await pool.query(`
+    DROP TABLE items;
+    DROP TABLE categories;
+    `)
 
   initializeDatabase()
     .then(() => {
@@ -154,4 +352,4 @@ if (isMainModule) {
       console.error("Server startup failed:", error);
       process.exit(1);
     });
-}
+// }
